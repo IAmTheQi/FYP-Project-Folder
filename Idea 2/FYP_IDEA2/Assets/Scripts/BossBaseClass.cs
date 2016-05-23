2a0e5983a3df3f8efd411594f0fd7673dc682ea2
OGXiaoQi <xiaoqi.loh@gmail.com>
2016-05-23 16:52:58 +0800

    Idea 2 Changes

---
+++ Idea 2/FYP_IDEA2/Assets/Scripts/BossBaseClass.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossBaseClass: MonoBehaviour{
+
+    public float health;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public float TakeDamage(float value)
+    {
+        if ((health - value) > 0)
+        {
+            health -= value;
+        }
+        else if ((health - value) < 0)
+        {
+            health = 0;
+        }
+
+        return health;
+    }
+
+    public virtual void Attack()
+    {
+
+    }
+}
---
+++ Idea 2/FYP_IDEA2/Assets/Scripts/BossOne.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossOne : BossBaseClass {
+
+	// Use this for initialization
+	void Start () {
+        health = 200.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
