5f4ee8f76aad59e7174ba6e8bad2cedecc332f71
OGXiaoQi <xiaoqi.loh@gmail.com>
2017-01-06 15:23:00 +0800

    Implemented bullet marks, mutant blood splat particle effects
    Changed: GunShotFeedbackHandler script, Bullet Marks prefab

---
+++ FYP_IDEA1/Assets/Scripts/GunShotFeedbackHandler.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunShotFeedbackHandler : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public ParticleSystem[] bloodPrefabArray;
+
+    GameObject[] bulletMarkArray;
+    int bulletMarkIndex;
+
+    ParticleSystem[] bloodSplatArray;
+    int bloodSplatIndex;
+
+	// Use this for initialization
+	void Start () {
+
+        bulletMarkArray = new GameObject[20];
+        bulletMarkIndex = 0;
+	
+        for (int i = 0; i < bulletMarkArray.Length; i++)
+        {
+            bulletMarkArray[i] = Instantiate(bulletPrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+            bulletMarkArray[i].SetActive(false);
+        }
+
+        bloodSplatArray = new ParticleSystem[20];
+        bloodSplatIndex = 0;
+
+        for (int j = 0; j < bloodSplatArray.Length; j++)
+        {
+            int index = Random.Range(0, (bloodPrefabArray.Length - 1));
+            bloodSplatArray[j] = Instantiate(bloodPrefabArray[index], new Vector3(0, 0, 0), Quaternion.identity) as ParticleSystem;
+            bloodSplatArray[j].gameObject.SetActive(false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void CreateMark(bool mutantHit, RaycastHit hit)
+    {
+        if (!mutantHit)
+        {
+            bulletMarkArray[bulletMarkIndex].SetActive(false);
+            bulletMarkArray[bulletMarkIndex].transform.position = hit.point + (hit.normal * 0.01f);
+            bulletMarkArray[bulletMarkIndex].transform.rotation = Quaternion.LookRotation(-hit.normal);
+            bulletMarkArray[bulletMarkIndex].SetActive(true);
+            Debug.LogFormat("index:{0}      normal:{1}", bulletMarkIndex, hit.normal);
+
+            if (bulletMarkIndex < bulletMarkArray.Length - 1)
+            {
+                bulletMarkIndex += 1;
+            }
+            else
+            {
+                bulletMarkIndex = 0;
+            }
+        }
+        else if (mutantHit)
+        {
+            bloodSplatArray[bloodSplatIndex].gameObject.SetActive(true);
+            bloodSplatArray[bloodSplatIndex].transform.position = hit.point;
+            bloodSplatArray[bloodSplatIndex].transform.rotation = Quaternion.LookRotation(-hit.normal);
+            bloodSplatArray[bloodSplatIndex].Emit(1);
+
+            if (bloodSplatIndex < bloodSplatArray.Length - 1)
+            {
+                bloodSplatIndex += 1;
+            }
+            else
+            {
+                bloodSplatIndex = 0;
+            }
+        }
+    }
+}
---
+++ FYP_IDEA1/Assets/Scripts/TriggerActivate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerActivate : MonoBehaviour {
+
+    public GameObject[] targets;
+
+	// Use this for initialization
+	void Start () {
+
+        for (int i = 0; i < targets.Length; i++)
+        {
+            targets[i].SetActive(false);
+        }
+	
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            for (int i = 0; i < targets.Length; i++)
+            {
+                targets[i].SetActive(true);
+            }
+        }
+    }
+}
